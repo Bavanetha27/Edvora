07a20c29e465e8e0314415fcf4bbe6c2080c3cea
Bavanetha M R <bavanethamr@gmail.com>
2025-10-17 21:43:22 +0530

    cors error rectified

--- server/server.js
+++ server/server.js
@@ -5,16 +5,43 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const quizRoutes = require("./routes/quizRoutes");
 const aiRoutes = require("./routes/aiRoutes");
-const cookieParser =  require("cookie-parser");
+const cookieParser = require("cookie-parser");
 
 dotenv.config();
 const app = express();
 connectDB();
 
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
+//CORS middleware BEFORE routes
+const allowedOrigins = [
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+//handle preflight requests globally
+app.options("*", cors({
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}));
+
+// Routes
 app.use("/auth", authRoutes);
 app.use("/quiz", quizRoutes);
 app.use("/ai", aiRoutes);
